8f9abde5e6bbab34753790d1906e613dda089c11
Quân Nguyễn Hoàng <nguyenhoangquan149@gmail.com>
2023-03-27 22:37:20 +0700

    add file index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
+import 'index.css'
 
 if (document.getElementById('root')) {
     ReactDOM.render(
-        <Layout/>,
-        document.getElementById('root'));
+        <Layout />,
+        document.getElementById('root')
+    );
 }
